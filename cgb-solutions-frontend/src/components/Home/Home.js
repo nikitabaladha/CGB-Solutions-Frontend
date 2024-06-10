76d9d5fabc793b3ac3cf2178246a51918fb704f5
nikitabaladha <nikitabaladha1996@gmail.com>
2024-06-10 22:57:05 +0530

    Home with image,  summary and all things

--- cgb-solutions-frontend/src/components/Home/Home.js
+++ cgb-solutions-frontend/src/components/Home/Home.js
@@ -5,27 +5,23 @@ import "./Home.css";
 import getAPI from "../../Api/axiosGet";
 import Navbar from "../Navbar/Navbar.js";
 
-const convertPath = (path) => {
-  return path.replace(/\\/g, "/");
-};
-
 const Home = () => {
-  const [blog, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
         const response = await getAPI("/blog", false);
-        const blogsWithForwardSlashes = response.data.data.map((blog) => ({
+        const modifiedBlogs = response.data.data.map((blog) => ({
           ...blog,
-          bannerImageUrl: convertPath(blog.bannerImageUrl),
+          date: new Date(blog.date).toISOString().split("T")[0],
         }));
-        console.log(response.data.data);
-        setBlogs(response.data.data);
+        setBlogs(modifiedBlogs);
+        console.log(modifiedBlogs);
       } catch (err) {
         setError(err.message);
-        console.error("Error fetching banners:", err);
+        console.error("Error fetching blogs:", err);
       }
     };
 
@@ -37,15 +33,34 @@ const Home = () => {
       <Navbar />
       <div className="blog-container">
         {error && <p className="error">Error: {error}</p>}
-        {blog.length === 0 ? (
+        {blogs.length === 0 ? (
           <p>No blogs available</p>
         ) : (
-          blog.map((blog) => (
+          blogs.map((blog) => (
             <div key={blog._id} className="blog">
-              <img src={blog.bannerImageUrl} alt={blog.title} />
-
-              <h2>{blog.title}</h2>
-              <p>{blog.summary}</p>
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="image-container">
+                    <img
+                      src={blog.bannerImageUrl}
+                      alt={blog.title}
+                      className="img-fluid"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="blog-content">
+                    <h3>{blog.title}</h3>
+                    <p>
+                      <strong>{blog.userName}</strong> | {blog.date}
+                    </p>
+                    <div
+                      className="summary"
+                      dangerouslySetInnerHTML={{ __html: blog.summary }}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           ))
         )}
